fb5c0ed6e595e13e3d197f643b0ceadef2c4c8ed
JonNir <jonathan.nir@mail.huji.ac.il>
2024-02-19 18:53:04 +0200

    add event duration to experiment_config.py

--- Config/experiment_config.py
+++ Config/experiment_config.py
@@ -5,6 +5,12 @@ This file contains the configuration for each specific experiment.
 import os
 from Config.ScreenMonitor import ScreenMonitor
 from Config.ExperimentTriggerEnum import ExperimentTriggerEnum
+from Config.GazeEventTypeEnum import GazeEventTypeEnum
+
+# GLOBAL VARIABLES
+SCREEN_MONITOR: ScreenMonitor = ScreenMonitor.from_tobii_default()  # global variable: screen monitor object
+TRIGGERS = ExperimentTriggerEnum  # global variable: triggers enum
+EVENTS = GazeEventTypeEnum  # global variable: events enum
 
 # DIRECTORIES
 BASE_DIR = ""
@@ -12,7 +18,11 @@ STIMULI_DIR = os.path.join(BASE_DIR, "Stimuli", "generated_stim1")
 RAW_DATA_DIR = os.path.join(BASE_DIR, "RawData")
 OUTPUT_DIR = os.path.join(BASE_DIR, "Results")
 
-# GLOBAL VARIABLES
-SCREEN_MONITOR: ScreenMonitor = ScreenMonitor.from_tobii_default()  # global variable: screen monitor object
-TRIGGERS = ExperimentTriggerEnum  # global variable: triggers enum
+# GAZE-EVENT VALUES
+EVENT_DURATIONS = {
+    EVENTS.BLINK: (10, 250),
+    EVENTS.FIXATION: (50, 2000),
+    EVENTS.SACCADE: (10, 250)
+}  # duration range for each event type
+
 EXPERIMENT_SPECIFIC_VARIABLES = []  # additional variable recorded in the experiment and extracted from the raw data
