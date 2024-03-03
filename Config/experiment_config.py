1ead3b395acbaa1e0cc12f95ad001fea7e5e6196
JonNir <jonathan.nir@mail.huji.ac.il>
2024-03-03 17:17:15 +0200

    change PSO max duration based on value in https://shorturl.at/fmwUW

--- Config/experiment_config.py
+++ Config/experiment_config.py
@@ -15,7 +15,7 @@ EVENT_MAPPING = {
     cnst.EVENTS.UNDEFINED: {"label": "Undefined", "color": "#dddddd", "min_duration": 0, "max_duration": 1e6},
     cnst.EVENTS.FIXATION: {"label": "Fixation", "color": "#1f78b4", "min_duration": 50, "max_duration": 2000},
     cnst.EVENTS.SACCADE: {"label": "Saccade", "color": "#33a02c", "min_duration": 10, "max_duration": 250},
-    cnst.EVENTS.PSO: {"label": "PSO", "color": "#b2df8a", "min_duration": 10, "max_duration": 250},
+    cnst.EVENTS.PSO: {"label": "PSO", "color": "#b2df8a", "min_duration": 10, "max_duration": 80},
     cnst.EVENTS.SMOOTH_PURSUIT: {"label": "Smooth Pursuit", "color": "#fb9a99", "min_duration": 10, "max_duration": 2000},
     cnst.EVENTS.BLINK: {"label": "Blink", "color": "#222222", "min_duration": 10, "max_duration": 2000}
 }
