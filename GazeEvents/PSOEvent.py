0d20f857776b98c083257ee6b6f572c9b2999df3
JonNir <jonathan.nir@mail.huji.ac.il>
2024-03-10 20:30:23 +0200

    del unused

--- GazeEvents/PSOEvent.py
+++ GazeEvents/PSOEvent.py
@@ -1,10 +1,6 @@
 import numpy as np
-import pandas as pd
-from typing import Tuple
 
 import Config.constants as cnst
-import Utils.pixel_utils as pixel_utils
-import Utils.visual_angle_utils as visang_utils
 from GazeEvents.BaseGazeEvent import BaseGazeEvent
 
 
