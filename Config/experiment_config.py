c9c0152205e91f3d2a252f42cce54ea5fce3b7fd
JonNir <jonathan.nir@mail.huji.ac.il>
2024-03-10 18:49:57 +0200

    change defaults

--- Config/experiment_config.py
+++ Config/experiment_config.py
@@ -17,8 +17,8 @@ EVENT_MAPPING = {
     cnst.EVENTS.FIXATION: {"label": "Fixation", "color": "#1f78b4", "min_duration": 50, "max_duration": 2000},
     cnst.EVENTS.SACCADE: {"label": "Saccade", "color": "#33a02c", "min_duration": 10, "max_duration": 250},
     cnst.EVENTS.PSO: {"label": "PSO", "color": "#b2df8a", "min_duration": 10, "max_duration": 80},
-    cnst.EVENTS.SMOOTH_PURSUIT: {"label": "Smooth Pursuit", "color": "#fb9a99", "min_duration": 10, "max_duration": 2000},
-    cnst.EVENTS.BLINK: {"label": "Blink", "color": "#222222", "min_duration": 10, "max_duration": 2000}
+    cnst.EVENTS.SMOOTH_PURSUIT: {"label": "Smooth Pursuit", "color": "#fb9a99", "min_duration": 40, "max_duration": 2000},
+    cnst.EVENTS.BLINK: {"label": "Blink", "color": "#222222", "min_duration": 20, "max_duration": 2000}
 }
 
 DEFAULT_MISSING_VALUE = np.nan  # default value for missing data
