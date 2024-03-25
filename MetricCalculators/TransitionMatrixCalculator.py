c6e9d369ac49bcd9686b7f747cb90f2e1018f2b3
jonnir <nir.jonathan@gmail.com>
2024-03-25 22:48:04 +0200

    add more metrics

--- MetricCalculators/TransitionMatrixCalculator.py
+++ MetricCalculators/TransitionMatrixCalculator.py
@@ -29,3 +29,47 @@ def transition_probabilities(seq: Sequence) -> np.ndarray:
     row_sum = counts.sum(axis=1, keepdims=True)
     probs = np.divide(counts, row_sum, out=np.zeros_like(counts, dtype=float), where=row_sum != 0)
     return probs
+
+
+def calculate_stationary_distribution(probs: np.ndarray, allow_zero: bool = True) -> np.ndarray:
+    """
+    Calculate the stationary distribution of the given transition matrix.
+    The stationary distribution is the left-eigenvector (π) of the transition matrix (P), so πP = π.
+    See detailed explanation: https://stackoverflow.com/a/58334399/8543025
+    """
+    eigenvalues, eigenvectors = np.linalg.eig(probs.T)
+    stationary = np.array(eigenvectors[:, np.where(np.abs(eigenvalues - 1.) < cnst.EPSILON)[0][0]].flat)
+    stationary = (stationary / np.sum(stationary)).real
+    stationary[stationary < 0] = 0
+    if not allow_zero:
+        stationary[stationary == 0] = 0.01 * cnst.EPSILON
+    stationary /= np.sum(stationary)
+    return stationary
+
+
+def matrix_distance(m1: np.ndarray, m2: np.ndarray, norm: str = "fro") -> float:
+    """ Calculate the distance between two matrices. """
+    norm = norm.lower()
+    if norm == "fro" or norm == "frobenius" or norm == "euclidean" or norm == "l2":
+        return np.linalg.norm(m1 - m2, ord="fro")
+    if norm == "l1" or norm == "manhattan":
+        return np.linalg.norm(m1 - m2, ord=1)
+    if norm == "linf" or norm == "infinity" or norm == "max" or norm == np.inf:
+        return np.linalg.norm(m1 - m2, ord=np.inf)
+    if norm == "kl" or norm == "kullback-leibler":
+        stationary1 = __extract_stationary(m1)
+        stationary2 = __extract_stationary(m2)
+        return np.sum(stationary1 * np.log(stationary1 / stationary2))
+    raise ValueError(f"Invalid norm: {norm}")
+
+
+def __extract_stationary(m: np.ndarray) -> np.ndarray:
+    if m.ndim == 2 and m.shape[0] == m.shape[1]:
+        stationary = calculate_stationary_distribution(m, allow_zero=False)
+    elif m.ndim == 2 and 1 in m.shape:
+        stationary = m.flatten()
+    elif m.ndim == 1:
+        stationary = m
+    else:
+        raise ValueError(f"Invalid matrix shape {m.shape}")
+    return stationary
