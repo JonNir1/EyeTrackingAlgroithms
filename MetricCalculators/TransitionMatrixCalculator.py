3601cca3d22f0ee31e9d78355457732dcf700f4c
jonnir <nir.jonathan@gmail.com>
2024-03-25 20:12:54 +0200

    break into 2 different functions

--- MetricCalculators/TransitionMatrixCalculator.py
+++ MetricCalculators/TransitionMatrixCalculator.py
@@ -2,22 +2,30 @@ import numpy as np
 from typing import Sequence
 
 import Config.constants as cnst
+import Config.helpers as hlp
 from GazeEvents.BaseEvent import BaseEvent
 
 _NUM_EVENTS = len(cnst.EVENT_LABELS)
 
 
-def calculate_transition_matrix(seq: Sequence) -> np.ndarray:
-    if all(isinstance(e, BaseEvent) for e in seq):
-        seq = [e.event_type() for e in seq]
-    if not all(isinstance(e, cnst.EVENT_LABELS) for e in seq):
-        raise ValueError("Sequence must be of the same type (`GazeEventTypeEnum` or `BaseEvent`)")
-    matrix = np.zeros((_NUM_EVENTS, _NUM_EVENTS), dtype=int)
+def transition_counts(seq: Sequence) -> np.ndarray:
+    """ Count the number of transitions between events in the given sequence. """
+    seq = [e.event_type() if isinstance(e, BaseEvent) else hlp.parse_gaze_event(e, safe=False) for e in seq]
+    counts = np.zeros((_NUM_EVENTS, _NUM_EVENTS), dtype=int)
     for i in range(len(seq) - 1):
         from_event = seq[i]
         to_event = seq[i + 1]
-        matrix[from_event][to_event] += 1
-    # calculate row-wise probabilities (i.e. P[to_col|from_row]); avoid division by zero
-    row_sum = matrix.sum(axis=1, keepdims=True)
-    probs = np.divide(matrix, row_sum, out=np.zeros_like(matrix, dtype=float), where=row_sum != 0)
-    return matrix
+        counts[from_event][to_event] += 1
+    return counts
+
+
+def transition_probabilities(seq: Sequence) -> np.ndarray:
+    """
+    Calculate the transition probabilities between events in the given sequence.
+    Returns a matrix where each row represents the current event and each column represents the next event, so cells
+    contain the probability P(to_column | from_row).
+    """
+    counts = transition_counts(seq)
+    row_sum = counts.sum(axis=1, keepdims=True)
+    probs = np.divide(counts, row_sum, out=np.zeros_like(counts, dtype=float), where=row_sum != 0)
+    return probs
