57b633bd5e73f6a28d788df7887ca19401d51b02
JonNir <jonathan.nir@mail.huji.ac.il>
2024-04-07 17:29:08 +0300

    compare Tav's filter with original srp filter

--- ProjectTAV/tav_playground.py
+++ ProjectTAV/tav_playground.py
@@ -5,6 +5,7 @@ from pymatreader import read_mat
 import scipy.signal as signal
 import pywt
 import plotly.express as px
+import plotly.graph_objects as go
 import plotly.io as pio
 
 import ProjectTAV.tav_helpers as tavh
@@ -12,6 +13,18 @@ from ProjectTAV.Subject import Subject
 
 
 pio.renderers.default = "browser"
+
+#################################
+# Difference between original SRP filter and TAV's SRP filter
+#################################
+
+srp = tavh.create_filter('srp')
+srp_tav = tavh.create_filter('srp_tav')
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(y=srp[0], mode='lines', name='SRP'))
+fig.add_trace(go.Scatter(y=srp_tav[0], mode='lines', name='SRP TAV'))
+fig.show()
 
 #################################
 
