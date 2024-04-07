46880d71804454e132400a7c31994d0461497f85
JonNir <jonathan.nir@mail.huji.ac.il>
2024-04-07 17:29:08 +0300

    compare Tav's filter with original srp filter

--- ProjectTAV/tav_playground.py
+++ ProjectTAV/tav_playground.py
@@ -16,6 +16,18 @@ from ProjectTAV.tav_participant import Participant
 pio.renderers.default = "browser"
 
 #################################
+# Difference between original SRP filter and TAV's SRP filter
+#################################
+
+srp = tavh.create_filter('srp')
+srp_tav = tavh.create_filter('srp_tav')
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(y=srp[0], mode='lines', name='SRP'))
+fig.add_trace(go.Scatter(y=srp_tav[0], mode='lines', name='SRP TAV'))
+fig.show()
+
+#################################
 
 s101 = Subject(101)
 p101 = Participant(101)
