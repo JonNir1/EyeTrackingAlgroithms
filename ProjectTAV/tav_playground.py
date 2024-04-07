73e3f1bf023e33d071fe4cb601cabe4d24f82ff4
JonNir <jonathan.nir@mail.huji.ac.il>
2024-04-07 19:01:39 +0300

    fix tav filter

--- ProjectTAV/tav_playground.py
+++ ProjectTAV/tav_playground.py
@@ -10,25 +10,16 @@ import plotly.io as pio
 
 import ProjectTAV.tav_helpers as tavh
 from ProjectTAV.Subject import Subject
+from ProjectTAV.tav_participant import Participant
 
 
 pio.renderers.default = "browser"
 
 #################################
-# Difference between original SRP filter and TAV's SRP filter
-#################################
-
-# srp = tavh.create_filter('srp')
-# srp_tav = tavh.create_filter('srp_tav')
-#
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(y=srp[0], mode='lines', name='SRP'))
-# fig.add_trace(go.Scatter(y=srp_tav[0], mode='lines', name='SRP TAV'))
-# fig.show()
-
-#################################
 
 s101 = Subject(101)
+p101 = Participant(101)
+
 timestamp_idxs = np.arange(s101.num_samples)
 reog = s101.calculate_radial_eog()
 srp_reog = tavh.apply_filter(reog, 'srp_tav')
