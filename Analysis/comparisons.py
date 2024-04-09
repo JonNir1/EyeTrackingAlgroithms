3c142ba5cdb3ab7da7da03375afe6e7b51b52fa5
jonnir <nir.jonathan@gmail.com>
2024-04-09 21:16:54 +0300

    allow no grouping

--- Analysis/comparisons.py
+++ Analysis/comparisons.py
@@ -224,14 +224,15 @@ def matched_events_feature_difference(events: pd.DataFrame,
     return group_and_aggregate(contrast, group_by)
 
 
-def group_and_aggregate(data: pd.DataFrame, group_by: Optional[str] = None) -> pd.DataFrame:
+def group_and_aggregate(data: pd.DataFrame, group_by: Optional[Union[str, List[str]]]) -> pd.DataFrame:
     """ Group the data by the given criteria and aggregate the values in each group. """
-    # create a Series of all measure values in each column
-    group_all = pd.Series([data[col].explode().to_list() for col in data.columns], index=data.columns, name="all")
     if group_by is None:
-        return pd.DataFrame(group_all).T
-    # create a list of values per group & column, and add a row for "all" group
+        return data
     grouped_vals = data.groupby(level=group_by).agg(list).map(lambda group: pd.Series(group).explode().to_list())
+    if len(grouped_vals.index.names) == 1:
+        return grouped_vals
+    # there is more than one group, so add a row for "all" groups
+    group_all = pd.Series([data[col].explode().to_list() for col in data.columns], index=data.columns, name="all")
     grouped_vals = pd.concat([grouped_vals.T, group_all], axis=1).T  # add "all" row
     return grouped_vals
 
