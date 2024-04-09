2cc4d7ba1cf07da39403a0eaf9af9cb1cae8272c
jonnir <nir.jonathan@gmail.com>
2024-04-09 23:30:19 +0300

    add inter-event comparison metric

--- GazeEvents/BaseGazeEvent.py
+++ GazeEvents/BaseGazeEvent.py
@@ -45,6 +45,15 @@ class BaseGazeEvent(BaseEvent):
         # TODO: check if inside the screen
         return reasons
 
+    def l2_timing_offset(self, other):
+        """
+        Event-matching metric: L2 norm of the timing offset between two events.
+        See Kothari et al. (2020) for more details.
+        """
+        onset_diff = self.start_time - other.start_time
+        offset_diff = self.end_time - other.end_time
+        return np.sqrt(onset_diff ** 2 + offset_diff ** 2)
+
     @final
     @property
     def start_point(self) -> Tuple[float, float]:
