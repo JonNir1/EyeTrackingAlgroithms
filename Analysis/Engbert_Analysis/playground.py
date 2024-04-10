982bf5cbc78172261927cf6e0048bb6ec3c0593b
jonnir <nir.jonathan@gmail.com>
2024-04-11 00:02:37 +0300

    start 2nd refactor

--- Analysis/Engbert_Analysis/playground.py
+++ Analysis/Engbert_Analysis/playground.py
@@ -1,3 +1,32 @@
+import numpy as np
+import pandas as pd
 
-# TODO: Use helpers.py instead of comparisons.py
+import Config.constants as cnst
+from GazeDetectors.EngbertDetector import EngbertDetector
+
+import Analysis.helpers as hlp
+import Analysis.figures as figs
+
+DATASET = "Lund2013"
+LAMBDA_STR = "λ"
+COL_MAPPER = lambda col: col[col.index(LAMBDA_STR):col.index(",")].replace("'", "") if LAMBDA_STR in col else col
+DETECTORS = [EngbertDetector(lambdaa=lmda) for lmda in np.arange(7)]
+
+# %%
+samples_df, events_df, detector_results_df, matches, comparison_columns = hlp.preprocess_dataset(DATASET,
+                                                                                                 column_mapper=COL_MAPPER,
+                                                                                                 verbose=True)
+
+# %%
+# Velocity-Threshold Distribution
+thresholds = pd.concat([detector_results_df[f"{LAMBDA_STR}:0"].map(lambda cell: cell['thresh_Vx']),
+                        detector_results_df[f"{LAMBDA_STR}:0"].map(lambda cell: cell['thresh_Vy'])],
+                       axis=1, keys=["Vx", "Vy"])
+agg_thresholds = hlp.group_and_aggregate(thresholds, group_by=cnst.STIMULUS)
+threshold_distribution_fig = figs.distributions_grid(agg_thresholds,
+                                                     title="Velocity-Threshold Distribution")
+threshold_distribution_fig.show()
+
+
+# TODO: copy from notebook to here and use helpers.py instead of comparisons.py
 
