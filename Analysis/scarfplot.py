4aed3d937dd09805387212e5ab4a7a44e0b2fc3d
jonnir <nir.jonathan@gmail.com>
2024-04-12 10:06:39 +0300

    add multi-scarf figure

--- Analysis/scarfplot.py
+++ Analysis/scarfplot.py
@@ -5,6 +5,31 @@ import Config.constants as cnst
 import Config.experiment_config as cnfg
 
 
+def compare_scarfplots(t: np.ndarray, *events, **kwargs) -> go.Figure:
+    """
+    Creates a figure with multiple scarfplots stacked on top of each other.
+    :param t: The time axis.
+    :param events: The events to be plotted.
+
+    :keyword names: The names of the scarfplots, defaults to [0, 1, 2, ...].
+    :keyword scarf_size: The width (in y-axis units) of each scarfplot, defaults to 1.
+    :keyword colorbar_length: The length of the colorbar (range [0, 1] where 1 is the full height of the plot), defaults to 0.75.
+    :keyword colorbar_thickness: The thickness of the colorbar, defaults to 25.
+    :keyword colorbar_show: Whether to show the colorbar, defaults to True.
+
+    :return: The figure with the scarfplots.
+    """
+    num_scarfs = len(events)
+    names = kwargs.get("names", [str(i) for i in range(num_scarfs)])
+    scarf_size = kwargs.get("scarf_size", 1)
+    assert len(names) == num_scarfs
+    fig = go.Figure()
+    for i, e in enumerate(events):
+        ymin, ymax = 2 * i * scarf_size, (2 * i + 1) * scarf_size
+        fig = add_scarfplot(fig, t, e, ymin, ymax, **kwargs)
+    return fig
+
+
 def add_scarfplot(fig: go.Figure,
                   t: np.ndarray,
                   events: np.ndarray,
@@ -23,12 +48,12 @@ def add_scarfplot(fig: go.Figure,
         zmax=np.nanmax([e.value for e in cnst.EVENT_LABELS]),
         colorscale=colormap,
         colorbar=dict(
-            len=colorbar_kwargs.get("len", 0.75),
-            thickness=colorbar_kwargs.get("thickness", 25),
+            len=colorbar_kwargs.get("colorbar_length", 0.75),
+            thickness=colorbar_kwargs.get("colorbar_thickness", 25),
             tickvals=centers,
             ticktext=[e.name for e in cnst.EVENT_LABELS],
         ),
-        showscale=colorbar_kwargs.get("showscale", True),
+        showscale=colorbar_kwargs.get("colorbar_show", True),
     )
     fig.add_trace(scarfplot)
     return fig
