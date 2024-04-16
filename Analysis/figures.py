33d331caf8dea9b6bf1572070f2aedac02460842
jonnir <nir.jonathan@gmail.com>
2024-04-16 14:37:55 +0300

    move figs to Visualization dir [1/n]

--- Analysis/figures.py
+++ Analysis/figures.py
@@ -1,32 +1,6 @@
-import numpy as np
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
-import Config.constants as cnst
-
-_HEATMAP_COLORMAP = "HOT"
-
-
-def similarity_heatmap(data: pd.DataFrame, title: str, similarity_measure: str) -> go.Figure:
-    fig = px.imshow(data.T,
-                    title=title,
-                    color_continuous_scale=_HEATMAP_COLORMAP,
-                    labels=dict(x=cnst.TRIAL.capitalize(), y="Detectors", color=similarity_measure),
-                    x=data.index.get_level_values(cnst.TRIAL),
-                    aspect="auto")
-    fig.update_layout(
-        xaxis=dict(side="top"),
-        yaxis=dict(tickmode="array",
-                   tickvals=np.arange(len(data.columns)),
-                   ticktext=data.columns.tolist()
-                   ),
-        coloraxis=dict(colorbar_x=1.,
-                       colorbar_title_side='right',
-                       ),
-    )
-    return fig
 
 
 def distributions_grid(data: pd.DataFrame, **kwargs) -> go.Figure:
---
+++ Visualization/detectors_similarity_heatmap.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+import Config.constants as cnst
+
+
+_HEATMAP_COLORMAP = "HOT"
+
+
+def similarity_heatmap(data: pd.DataFrame, title: str, similarity_measure: str) -> go.Figure:
+    fig = px.imshow(data.T,
+                    title=title,
+                    color_continuous_scale=_HEATMAP_COLORMAP,
+                    labels=dict(x=cnst.TRIAL.capitalize(), y="Detectors", color=similarity_measure),
+                    x=data.index.get_level_values(cnst.TRIAL),
+                    aspect="auto")
+    fig.update_layout(
+        xaxis=dict(side="top"),
+        yaxis=dict(tickmode="array",
+                   tickvals=np.arange(len(data.columns)),
+                   ticktext=data.columns.tolist()
+                   ),
+        coloraxis=dict(colorbar_x=1.,
+                       colorbar_title_side='right',
+                       ),
+    )
+    return fig
+
