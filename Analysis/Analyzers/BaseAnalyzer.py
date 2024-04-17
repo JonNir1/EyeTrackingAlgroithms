103ff09adebd452a215797d0273f0d799d30867b
jonnir <nir.jonathan@gmail.com>
2024-04-17 18:25:49 +0300

    change Analyzers [2/n]

--- Analysis/Analyzers/BaseAnalyzer.py
+++ Analysis/Analyzers/BaseAnalyzer.py
@@ -113,34 +113,6 @@ class BaseAnalyzer(ABC):
                              **kwargs) -> pd.DataFrame:
         raise NotImplementedError
 
-    @classmethod
-    def analyze_impl(cls,
-                     events_df: pd.DataFrame,
-                     ignore_events: Set[cnfg.EVENT_LABELS] = None,
-                     verbose: bool = False,
-                     **kwargs):
-        """
-        Analyze the given events DataFrame and extract the features of the detected events.
-
-        :param events_df: A DataFrame containing the detected events of each rater/detector.
-        :param ignore_events: A set of event labels to ignore when extracting the features.
-        :param verbose: Whether to print the progress of the analysis.
-        :param kwargs: placeholder for additional parameters used by inherited classes.
-
-        :return: A dictionary mapping a feature name to a DataFrame containing the extracted features.
-        """
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            start = time.time()
-            ignore_events = ignore_events or set()
-            results = {BaseAnalyzer.EVENT_FEATURES_STR: BaseAnalyzer._extract_event_features(events_df,
-                                                                                             ignore_events=ignore_events,
-                                                                                             verbose=verbose)}
-            end = time.time()
-            if verbose:
-                print(f"Total Analysis Time:\t{end - start:.2f}s")
-        return results
-
     @staticmethod
     def group_and_aggregate(data: pd.DataFrame,
                             group_by: Optional[Union[str, List[str]]] = cnst.STIMULUS) -> pd.DataFrame:
@@ -156,45 +128,6 @@ class BaseAnalyzer(ABC):
         return grouped_vals
 
     @staticmethod
-    def event_feature_statistical_comparison(feature_df: pd.DataFrame, test_name: str):
-        """
-        Performs a two-sample statistical test on the set of measured event-features between two raters/detectors.
-        :param feature_df: A DataFrame containing the extracted features of the events detected by each rater/detector.
-            Each column represents a different rater/detector, and each cell contains a list of the measured values.
-        :param test_name: The name of the statistical test to perform.
-        :return: A DataFrame containing the results of the statistical test between each pair of raters/detectors.
-        """
-        test_name = test_name.lower().replace("_", " ").replace("-", " ").strip()
-        if test_name in {"u", "u test", "mann whitney", "mann whitney u", "mannwhitneyu"}:
-            test_func = stat.mannwhitneyu
-        elif test_name in {"rank sum", "ranksum", "ranksums", "wilcoxon rank sum"}:
-            test_func = stat.ranksums
-        else:
-            raise ValueError(f"Unknown test name: {test_name}")
-
-        # calculate the statistical test for each pair of columns
-        feature_df = feature_df.map(lambda cell: [v for v in cell if not np.isnan(v)])
-        results = hlp.apply_on_column_pairs(feature_df, test_func, is_symmetric=True)
-
-        # remap the results to a DataFrame
-        statistics = {(col1, col2, cnst.STATISTIC): {vk: vv[0] for vk, vv in vals.items()}
-                      for (col1, col2), vals in results.items()}
-        p_values = {(col1, col2, cnst.P_VALUE): {vk: vv[1] for vk, vv in vals.items()}
-                    for (col1, col2), vals in results.items()}
-        results = {**statistics, **p_values}
-        results = pd.DataFrame(results)
-
-        # reorder columns to group by the first column, then the second column, and finally by the statistic type
-        column_order = {triplet: (
-            len([trp for trp in results.keys() if trp[0] == triplet[0]]),
-            len([trp for trp in results.keys() if trp[0] == triplet[1]]),
-            1 if triplet[2] == cnst.STATISTIC else 0
-        ) for triplet in results.keys()}
-        ordered_columns = sorted(results.columns, key=lambda col: column_order[col], reverse=True)
-        results = results[ordered_columns]
-        return results
-
-    @staticmethod
     def _get_default_detectors() -> Union[BaseDetector, List[BaseDetector]]:
         from GazeDetectors.IVTDetector import IVTDetector
         from GazeDetectors.IDTDetector import IDTDetector
@@ -203,77 +136,3 @@ class BaseAnalyzer(ABC):
         from GazeDetectors.REMoDNaVDetector import REMoDNaVDetector
         return [IVTDetector(), IDTDetector(), EngbertDetector(), NHDetector(), REMoDNaVDetector()]
 
-    @staticmethod
-    def _extract_event_features(events_df: pd.DataFrame,
-                                ignore_events: Set[cnfg.EVENT_LABELS] = None,
-                                verbose=False) -> Dict[str, pd.DataFrame]:
-        global_start = time.time()
-        if verbose:
-            print("Extracting Event Features...")
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            ignore_events = ignore_events or set()
-            results = {}
-            for feature in BaseAnalyzer.EVENT_FEATURES:
-                start = time.time()
-                if feature == "Count":
-                    grouped = BaseAnalyzer.__event_counts_impl(events_df, ignore_events=ignore_events)
-                elif feature == "Micro-Saccade Ratio":
-                    grouped = BaseAnalyzer.__microsaccade_ratio_impl(events_df)
-                else:
-                    attr = feature.lower().replace(" ", "_")
-                    feature_df = events_df.map(lambda cell: [getattr(e, attr) for e in cell if
-                                                             e.event_label not in ignore_events and hasattr(e, attr)])
-                    grouped = BaseAnalyzer.group_and_aggregate(feature_df)
-                results[feature] = grouped
-                end = time.time()
-                if verbose:
-                    print(f"\tExtracting {feature}s:\t{end - start:.2f}s")
-        global_end = time.time()
-        if verbose:
-            print(f"Total time:\t{global_end - global_start:.2f}s\n")
-        return results
-
-    @staticmethod
-    def __event_counts_impl(events: pd.DataFrame, ignore_events: Set[cnfg.EVENT_LABELS] = None, ) -> pd.DataFrame:
-        """
-        Counts the number of detected events for each detector by type of event, and groups the results by the stimulus.
-        :param events: A DataFrame containing the detected events of each rater/detector.
-        :return: A DataFrame containing the count of events detected by each rater/detector (cols), grouped by the given
-            criteria (rows).
-        """
-
-        def count_event_labels(data: List[Union[BaseEvent, cnfg.EVENT_LABELS]]) -> pd.Series:
-            labels = pd.Series([e.event_label if isinstance(e, BaseEvent) else e for e in data])
-            counts = labels.value_counts()
-            if counts.empty:
-                return pd.Series({l: 0 for l in cnfg.EVENT_LABELS})
-            if len(counts) == len(cnfg.EVENT_LABELS):
-                return counts
-            missing_labels = pd.Series({l: 0 for l in cnfg.EVENT_LABELS if l not in counts.index})
-            return pd.concat([counts, missing_labels]).sort_index()
-
-        ignore_events = ignore_events or set()
-        events = events.map(lambda cell: [e for e in cell if e.event_label not in ignore_events])
-        event_counts = events.map(count_event_labels)
-        grouped_vals = event_counts.groupby(level=cnst.STIMULUS).agg(list).map(sum)
-        if len(grouped_vals.index) == 1:
-            return grouped_vals
-        # there is more than one group, so add a row for "all" groups
-        group_all = pd.Series(event_counts.sum(axis=0), index=event_counts.columns, name="all")
-        grouped_vals = pd.concat([grouped_vals.T, group_all], axis=1).T  # add "all" row
-        return grouped_vals
-
-    @staticmethod
-    def __microsaccade_ratio_impl(events: pd.DataFrame,
-                                  threshold_amplitude: float = cnfg.MICROSACCADE_AMPLITUDE_THRESHOLD) -> pd.DataFrame:
-        saccades = events.map(lambda cell: [e for e in cell if e.event_label == cnfg.EVENT_LABELS.SACCADE])
-        saccades_count = saccades.map(len).to_numpy()
-        microsaccades = saccades.map(lambda cell: [e for e in cell if e.amplitude < threshold_amplitude])
-        microsaccades_count = microsaccades.map(len).to_numpy()
-
-        ratios = np.divide(microsaccades_count, saccades_count,
-                           out=np.full_like(saccades_count, fill_value=np.nan, dtype=float),  # fill NaN if denom is 0
-                           where=saccades_count != 0)
-        ratios = pd.DataFrame(ratios, index=events.index, columns=events.columns)
-        return BaseAnalyzer.group_and_aggregate(ratios)
