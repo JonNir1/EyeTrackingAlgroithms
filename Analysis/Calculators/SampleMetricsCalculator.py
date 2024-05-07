583a677446d99e6922f686e488190aa946db7faf
jonnir <nir.jonathan@gmail.com>
2024-05-07 19:13:45 +0300

    enable more TM metrics

--- Analysis/Calculators/SampleMetricsCalculator.py
+++ Analysis/Calculators/SampleMetricsCalculator.py
@@ -57,6 +57,10 @@ class SampleMetricsCalculator(BaseCalculator):
             metric_func = lambda s1, s2: metrics.transition_matrix_distance(s1, s2, norm="kl")
         elif met in {"fro", "frobenius", "l2", "transition matrix l2-norm"}:
             metric_func = lambda s1, s2: metrics.transition_matrix_distance(s1, s2, norm="fro")
+        elif met in {"l1", "manhattan", "transition matrix l1-norm"}:
+            metric_func = lambda s1, s2: metrics.transition_matrix_distance(s1, s2, norm="l1")
+        elif met in {"linf", "infinity", "max", "inf", "transition matrix l-infinity norm"}:
+            metric_func = lambda s1, s2: metrics.transition_matrix_distance(s1, s2, norm="linf")
         else:
             raise NotImplementedError(f"Unknown metric for samples:\t{metric_name}")
         computed = hlp.apply_on_column_pairs(samples, metric_func, is_symmetric=False)
